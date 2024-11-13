6f73aa6116235163981d673dbc9e77fabff58f25
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-11-14 05:28:23 +0900

    Update stdin.c

--- stdin.c
+++ stdin.c
@@ -5,9 +5,11 @@ int main() {
     scanf("%d", &n);
     printf("%d\n", n);
 
-    char data[n];
+    char data[n][100];
     for (int i = 0; i < n; i++) {
-        scanf("%s", &data[i]);
+        scanf("%s", data[i]);
+    }
+    for (int i = 0; i < n; i++) {
         printf("%s ", data[i]);
     }
 
