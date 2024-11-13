1383208732b7ae8afd777b72a4e9759b5f129e25
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-11-14 05:21:30 +0900

    Update stdin.cpp

--- stdin.cpp
+++ stdin.cpp
@@ -7,9 +7,11 @@ int main(void){
     cout << n << endl;
   
     vector <int> a(n);
-    for (int i=0; i<n; i++) {
-      cin >> a[i];
-      cout << n << "";
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
     }
     cout << endl;
   
