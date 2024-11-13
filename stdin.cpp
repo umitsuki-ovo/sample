0a7b45df12d3ca3d1c291ee8d50cfbcabf4156df
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-11-14 04:48:01 +0900

    Create stdin.cpp

---
+++ stdin.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int n;
+    cin >> n;
+    cout << n << endl;
+  
+    vector <int> a(n);
+    for (int i=0; i<n; i++) {
+      cin >> a[i];
+      cout << n << "";
+    }
+    cout << endl;
+  
+    return 0;
+}
