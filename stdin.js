9bbf728f26111f79f3487b5cbc3dd1d0dce4edcd
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-11-14 05:07:51 +0900

    Update stdin.js

--- stdin.js
+++ stdin.js
@@ -1,9 +1,12 @@
 function main(input){
-    const a = input.split("\n");  // ["2", "A B"]
-    const b = a[1].split(" "); // ["A", "B"]
-    
+    const a = input.split("\n");  // ["2", "A B", "C D"]
     console.log(a);
-    console.log(b);
+
+    let b = []
+    for (i = 1; i < a.length; i++){
+        b.push(a[i].split(" "));
+    }
+    console.log(b); // [["A", "B"], ["C", "D"]]
 }
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
